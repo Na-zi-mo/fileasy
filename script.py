8ae0a9726d95160100b74ee266523389c6c85b05
Na-zi-mo <nazimad2004@gmail.com>
2025-06-19 20:02:33 -0400

    improved gitignore

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-file = "passport_visa"
+file = "th-3612395174"
 
 img = Image.open(f"{file}.jpg")
-img.convert("RGB").save(f"{file}.pdf")
+img.convert("RGB").save(f"./runs/{file}.pdf")
